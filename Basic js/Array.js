1b8d1300660b7e3f27e77ab0e986c18ab93e46af
Faizan Ahmad Siddiqui <faizan14sid@gmail.com>
2021-07-10 23:39:18 +0530

    array method

---
+++ Basic js/Array.js
@@ -0,0 +1,18 @@
+//Manipulate Arrays With push()
+
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+myArray.push(["dog", 3]);
+console.log(myArray);
+
+//Manipulate Arrays With pop()
+
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray.pop();
+console.log(myArray);
